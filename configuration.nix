798e4dd0709b365339fbce6f4e9ed600d782ec2f
Marco Romano <mromanode@gmail.com>
2024-03-27 19:03:17 +0100

    fix(timeZone): fix typo.

--- configuration.nix
+++ configuration.nix
@@ -108,7 +108,7 @@
     networkmanager.enable = true;
   };
   
-  time.timeZone = "Europe/Amesterdam";
+  time.timeZone = "Europe/Amsterdam";
 
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
