e972972838ff9fa0fe9c73a21dcaed596f56b4ca
Marco Romano <mromanode@gmail.com>
2024-04-07 20:10:21 +0200

    feat: refactor configuration.

--- configuration.nix
+++ configuration.nix
@@ -1,27 +1,30 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 { config, lib, pkgs, ... }:
 
 {
-  # Hardware
   imports =
-    [
+    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  # Boot
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
-  # System
+
+  # Override filesystem.
   fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
+    { device = "/dev/disk/by-uuid/<uuid>";
       fsType = "ext4";
       options = [
          "noatime"
-	 "commit=60"
+	       "commit=60"
       ];
     };
   
+  # Override virtual memory. 
   boot.kernel.sysctl = {
     "vm.dirty_ratio" = 10;
     "vm.dirty_background_ratio" = 5;
@@ -55,13 +58,15 @@
     "net.ipv4.conf.all.send_redirects" = 0;
     "net.ipv4.conf.default.send_redirects" = 0;
   };
-
+  
+  # Override boot kernel parameters.
   boot.kernelParams = [
     "modprobe.blacklist=iTCO_wdt"
     "nowatchdog"
     "tcp_bbr"
   ];
-
+  
+  # Override udev rules.
   services.udev = {
     enable = true;
     extraRules = 
@@ -73,155 +78,207 @@
       ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", ATTR{mtu}="1500", ATTR{tx_queue_len}="2000"
       '';
   };
-
+  
+  # Install intel driver packages.
   hardware.opengl.extraPackages = with pkgs; [
     intel-compute-runtime
     intel-media-driver
   ];
   
+  # Enable driSupport.
   hardware.opengl.driSupport.enable = true;
-
+  
+  # Enable zram.
   zramSwap.enable = true;
-
+  
+  # Enable period fstrim.
   services.fstrim.enable = true;
-
+  
+  # Enable irqbalance.
   services.irqbalance.enable = true;
 
-  # Nix
-  nix.settings = { 
+  # Override Nix settings.
+  nix.settings = {
     experimental-features = "nix-command flakes";
     auto-optimise-store = true;
   };
 
-  # Nixpkgs
+  # Allow unfree packages.
   nixpkgs.config.allowUnfree = true;
-  
-  # Environment variables
-  environment.variables.EDITOR = "nvim";
 
-  # Graphical
-  services.xserver = {
-    enable = true;
-    videoDrivers = [ "modesetting" ];
-    xkb.layout = "us";
-    desktopManager.plasma5.enable = true;
-    displayManager.sddm.enable = true;
+  networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+  #   keyMap = "us";
+    useXkbConfig = true; # use xkb.options in tty.
   };
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.xserver.videoDrivers = [ "modesetting" ];
   environment.plasma5.excludePackages = with pkgs.libsForQt5; [
     pkgs.aha
     plasma-browser-integration
+    konsole
     oxygen
-    elisa
     (lib.getBin qttools)
   ];
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    audio.enable = true;
-    jack.enable = true;
-    pulse.enable = true;
-    wireplumber.enable = true;
-    socketActivation = true;
-  };
 
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-color-emoji
-    jetbrains-mono
+    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
   ];
 
   qt = {
     enable = true;
-    style = "breeze";
     platformTheme = "kde";
+    style = "breeze";
   };
 
-  programs.xwayland.enable = true;
-  
-  # Programs
-  programs.neovim = {
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Disable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Disable sound.
+  sound.enable = false;
+  hardware.pulseaudio.enable = false;
+
+  # Enable pipewire
+  services.pipewire = {
     enable = true;
-    defaultEditor = true;
+    audio.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+    alsa.enable = true;
+    wireplumber.enable = true;
+    socketActivation = true;
   };
 
-  programs.git.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.touchpad.tapping = false;
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.drinkwater = {
+   isNormalUser = true;
+   description = "drinkwater";
+   home = "/home/zhenta";
+   extraGroups = [ "wheel" "networkmanager" "kvm" "adbusers" ]; # Enable ‘sudo’ for the user.
+   openssh.authorizedKeys.keys = [
+     "ssh-ed25519 <some-public-key> drink@water.com"
+   ];
+  #   packages = with pkgs; [
+  #     firefox
+  #     tree
+  #   ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   wget
+   curl
+   lm_sensors
+   ethtool
+   pciutils
+   usbutils
+   file
+   which
+   gnused
+   gnutar
+   gawk
+   zstd
+   zip
+   xz
+   unzip
+   p7zip
+   python3
+   temurin-bin-21
+   scrcpy
+  ];
+
+  environment.variables.EDITOR = "neovim";
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
   programs.mtr.enable = true;
   programs.gnupg.agent = {
-    enable = true;
+    enable = true; 
     enableSSHSupport = true;
   };
-  
-  # Networking
-  networking = {
-    hostName = "nixos";
-    networkmanager.enable = true;
-  };
 
-  networking.firewall = {
-    enable = true;
-    # allowedTCPPorts = [ ... ];
-    # allowedUDPPorts = [ ... ]:
-  };
+  # Enable ADB.
+  programs.adb.enable = true;
 
-  time.timeZone = "Europe/Amsterdam";
+  # Enable xwayland
+  programs.xwayland.enable = true;
 
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
     settings = {
-      X11Forwarding = true;
-      PermitRootLogin = "no";
-      PasswordAuthentication = false;
+    X11Forwarding = true;
+      PermitRootLogin = "no"; # disable root login
+      PasswordAuthentication = false; # disable password login
     };
     openFirewall = true;
   };
 
-  
-  # Input
-  i18n.defaultLocale = "en_US.UTF-8";
-    console = {
-      font = "Lat2-Terminus16";
-      # keyMap = "us"; 
-      useXkbConfig = true;
-  };
-  
-  services.xserver.libinput = {
-    enable = true;
-    touchpad.tapping = false;
-  };
-  
-  # User
-  users.users.zhenta = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    openssh.authorizedKeys.keys = [
-        "ssh-ed25519 <some-public-key> drink@water"
-    ];
-  };
-  
-  # System packages
-  environment.systemPackages = with pkgs; [
-    wget
-    brave
-    temurin-bin-21
-    mpv
-    vscode
-    keepassxc
-    p7zip
-    jetbrains.idea-ultimate
-    transmission_4
-    python3
-    scrcpy
-    android-tools
-    lm_sensors
-    curl
-  ];
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 55252 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
 
-  # State Version
-  system.stateVersion = "23.11";
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = false;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
 
 }
 
